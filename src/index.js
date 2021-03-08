575e1e1f0d17d48dce5868f9ed276aa03ec783ab
HwangSunTae <pozafly@gmail.com>
2021-03-08 20:19:10 +0900

    thunk 적용

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
-import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  // logger를 사용하는 경우, logger가 가장 마지막에 와야 한다.
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+); // 여러개의 미들웨어를 적용할 수 있다.
 
 ReactDOM.render(
   <React.StrictMode>
--- src/middlewares/myLogger.js
+++ src/middlewares/myLogger.js
@@ -1,12 +1,9 @@
 const myLogger = (store) => (next) => (action) => {
-  console.group(action && action.type); // 액션 타입으로 log를 그룹화
-  console.log('이전 상태', store.getState());
-  console.log('액션', action); // 먼저 액션을 출력함
-
+  console.log(action); // 먼저 액션을 출력함
   const result = next(action); // 다음 미들웨어(또는 리듀서)에게 액션을 전달함
 
-  console.log('다음 상태', store.getState()); // '\t'는 탭 문자임.
-  console.groupEnd();
+  console.log('\t', store.getState()); // '\t'는 탭 문자임.
+
   return result; // 여기서 반환하는 값은 dispatch(action)의 결과물이 된다. 기본: undifined
 };
 
