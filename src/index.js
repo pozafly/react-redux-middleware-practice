dc619921e1a7f1a8f62406a6c4dd6fc1aa3a04e0
HwangSunTae <pozafly@gmail.com>
2021-03-11 11:53:41 +0900

    redux-saga

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { applyMiddleware, createStore } from 'redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import createSagaMiddleware from 'redux-saga'; // 사가 임포트
+
+const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만든다.
 
 const store = createStore(
   rootReducer,
   // logger를 사용하는 경우, logger가 가장 마지막에 와야 한다.
-  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
-); // 여러개의 미들웨어를 적용할 수 있다.
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware, // 사가 미들웨어 적용하고,
+      logger
+    )
+  )
+);
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해주자.
+// 주의 : 스토어 생성이 된 다음에 위 코드를 실행해야 함.
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router history={customHistory}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,18 +1,31 @@
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
 // 액션 타입
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
+const INCREASE_ASYNC = 'INCREASE_ASYNC';
+const DECREASE_ASYNC = 'DECREASE_ASYNC';
 
 // 액션 생성 함수
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-// getState를 쓰지 않는다면 굳이 파라미터로 받아올 필요 없다.
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => dispatch(increase()), 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => dispatch(decrease()), 1000);
-};
+// 리덕스-사가
+function* increaseSaga() {
+  yield delay(1000); // 1초를 기다린다.
+  yield put(increase()); // put은 특정 액션을 디스패치 해줌.
+}
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  yield takeEvery(INCREASE_ASYNC, increaseSaga); // 모든 INCREASE_ASYNC 액션을 처리
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga); // 가장 마지막으로 디스패치 된 DECREASE_ASYNC 액션만을 처리
+}
 
 // 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없음)
 const initialState = 0;
